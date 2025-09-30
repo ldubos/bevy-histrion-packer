1555dddf6a6109851ae16389bdbd082bc7812e6b
ldubos <ludwig.dubos@pm.me>
2025-10-01 00:48:22 +0200

    replace feature(doc_auto_cfg) with feature(doc_cfg)

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-#![cfg_attr(docsrs, feature(doc_auto_cfg))]
+#![cfg_attr(docsrs, feature(doc_cfg))]
 
 #[cfg(not(any(windows, unix)))]
 compile_error!("bevy-histrion-packer is not supported on this platform");
